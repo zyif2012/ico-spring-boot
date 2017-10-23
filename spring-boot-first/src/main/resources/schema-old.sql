adca25de8efce2227280b680be28c2c2e204c428
unknown <zyif2012@163.com>
2017-10-23 23:46:27 +0800

    modify

--- spring-boot-first/src/main/resources/schema-old.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE todo IF EXISTS;
-/* NOT NEEDED WHEN JPA IS ACTIVE
-CREATE TABLE todo(
-id SERIAL auto_increment primary key,
-user VARCHAR(255),
-desc VARCHAR(255),
-target_date TIMESTAMP,
-is_done BOOLEAN);
-*/
